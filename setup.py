6c8c69e834c226c5e70bf430e6305ff4e3579dd6
Mike Bertram <contact@mibexx.de>
2021-01-19 21:48:51 +0100

    * PyPi setup

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup
 
-setup(name='mibexx_gym_minesweeper',
-      version='0.0.1',
-      install_requires=['gym', 'numpy']
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mibexx_gym_minesweeper',
+    version='0.0.3',
+    license='MIT',
+    install_requires=['gym', 'numpy'],
+    author='Mike Bertram',
+    author_email='contact@mibexx.de',
+    description='Gym Environment for Minesweeper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords = ['GYM', 'ENVIRONMENT', 'MINESWEEPER', 'REINFORCEMENT LEARNING']
 )
